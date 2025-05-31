ce08c485432ccb246a6a29c9106ef2268f9b6108
Oscar <oscar.horn.roostami@outlook.com>
2025-05-31 22:49:41 +0200

    error fix on my-learning page

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,43 @@
-import React from 'react'
-import CompanionsList from '@/components/CompanionsList'
-import CTA from '@/components/CTA'
-import CompanionCard from '@/components/CompanionCard'
-import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.actions'
-import { getSubjectColor } from '@/lib/utils'
-
+import React from "react";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import CompanionCard from "@/components/CompanionCard";
+import {
+  getAllCompanions,
+  getRecentSessions,
+} from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
 const Page = async () => {
-  const companions = await getAllCompanions({ limit: 3});
+  const companions = await getAllCompanions({ limit: 3 });
   const reccentSessionsCompanions = await getRecentSessions(3);
 
   return (
     <main>
       <h1 className="text-2xl underline">Our top Companions</h1>
       <section className="home-section">
-        { companions.map(({id, subject, name, topic, duration, color}) => (
-          <CompanionCard key={id} id={id} subject={subject} name={name} topic={topic} duration={duration} color={getSubjectColor(subject)} />
+        {companions.map(({ id, subject, name, topic, duration, color }) => (
+          <CompanionCard
+            key={id}
+            id={id}
+            subject={subject}
+            name={name}
+            topic={topic}
+            duration={duration}
+            color={getSubjectColor(subject)}
+          />
         ))}
       </section>
       <section className="home-section">
-        <CompanionsList 
-          title="Recent Sessions"
+        <CompanionsList
+          title="Recently completed sessions"
           companions={reccentSessionsCompanions}
+          classNames="w-2/3 max-lg:w-full"
         />
         <CTA />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
